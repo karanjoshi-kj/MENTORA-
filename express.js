b883616648ea7a86e063372c27d6e748f839add7
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-30 21:28:56 +0530

    UPDATED DEPOLYMENT LINK

--- express.js
+++ express.js
@@ -11,6 +11,7 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/student", StudentRouter);
